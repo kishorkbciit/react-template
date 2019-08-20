2cffcda9d56d66e804a015e18509eb15a55375c0
Kishor <kishor@Kishors-MacBook-Pro.local>
2019-08-20 05:30:39 +0530

    add new component

---
+++ src/views/Base/LandingUrl/LandingUrl.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Badge, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+
+class LandingUrl extends Component {
+  render() {
+    return (
+      <div className="animated fadeIn">
+       This is the home page
+      </div>
+
+    );
+  }
+}
+
+export default LandingUrl;
